b0dbf9ada34cb48d9ee0095806f6e44acf0b1ae9
yokota.shuichi <yokota.shuichi@cy-com.co.jp>
2024-04-01 16:31:54 +0900

    sideメニューが空だったら子要素をそのまま表示

--- src/SideMenuPage.tsx
+++ src/SideMenuPage.tsx
@@ -1,18 +1,14 @@
-import React, { FC, useState } from 'react';
-import { Col, Drawer, IconButton, MenuItem, MenuList, Row, media } from '.';
+import React, { FC } from 'react';
+import { Col, MenuItem, MenuList, Row, media } from '.';
 
 // TODO mobileの時はGlobalMenuのDrawerにページメニューを表示してることを直感的に分からせたい
 interface Props {
-  menus: MenuItem[];
+  menus?: MenuItem[];
 }
 export const SideMenuPage: FC<React.PropsWithChildren<Props>> = (props) => {
   const isMobile = media.isLessThan('small');
-  return isMobile ? (
-    <>
-      <Row>
-        <Col>{props.children}</Col>
-      </Row>
-    </>
+  return isMobile || !props.menus ? (
+    <>{props.children}</>
   ) : (
     <Row>
       <Col large={1} medium={1} small={2}>
